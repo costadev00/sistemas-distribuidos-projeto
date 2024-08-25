f0bd1d87a6d33e969353b882a1dd5a28d187bc7a
ThwyIgo <thiagopachecorocha@hotmail.com>
2024-08-25 13:51:15 +0000

    gRPC funcionando

--- src/biblioteca/cad_client.py
+++ src/biblioteca/cad_client.py
@@ -1,14 +1,19 @@
 import sys
 
+import grpc
+
 from biblioteca import lib
+from biblioteca.gRPC import cadastro_pb2, cadastro_pb2_grpc
 
 def run():
     porta = int(sys.argv[1])
-    client = lib.connect_mqtt("client", porta)
-    client.subscribe("topico")
-    client.on_message = (lambda client, userdata, msg:
-        print(f"cad_client recebeu: {msg.payload.decode()}"))
-    client.loop_forever()
+    stub = connect_stub(porta)
+    status = stub.NovoUsuario(cadastro_pb2.Usuario(cpf="123.456.789-01", nome="Jão"))
+    print(status)
+
+def connect_stub(porta: int) -> cadastro_pb2_grpc.PortalCadastroStub:
+    channel = grpc.insecure_channel(f"localhost:{porta}")
+    return cadastro_pb2_grpc.PortalCadastroStub(channel)
 
 if __name__ == '__main__':
     run()
--- src/biblioteca/cad_server.py
+++ src/biblioteca/cad_server.py
@@ -1,11 +1,32 @@
+from concurrent import futures
 import sys
 
+import grpc
+
 from biblioteca import lib
+from biblioteca.gRPC import cadastro_pb2, cadastro_pb2_grpc
 
 def run():
+    if len(sys.argv) != 2:
+        print("Forneça a porta como argumento da linha de comando")
+        return
+    
     porta = int(sys.argv[1])
-    client = lib.connect_mqtt("server", porta)
-    client.publish("topico", "cad_server enviou mensagem")
+    serve(porta)
 
-if __name__ == '__main__':
+def serve(porta: int):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    cadastro_pb2_grpc.add_PortalCadastroServicer_to_server(PortalCadastroServicer(), server)
+    server.add_insecure_port(f"localhost:{porta}")
+    server.start()
+    server.wait_for_termination()
+
+class PortalCadastroServicer(cadastro_pb2_grpc.PortalCadastroServicer):
+    def NovoUsuario(self, request: cadastro_pb2.Usuario, context) -> cadastro_pb2.Status:
+        print(request)
+
+        status = cadastro_pb2.Status(status=0)
+        return status
+
+if __name__ == "__main__":
     run()
